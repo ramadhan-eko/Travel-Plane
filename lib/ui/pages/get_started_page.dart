f8145e214e772c5cc6ba670808ace5b51266c191
ramadhan-eko <ramadhaneko027@gmail.com>
2022-04-26 07:17:35 +0700

    slicing sign up

--- lib/ui/pages/get_started_page.dart
+++ lib/ui/pages/get_started_page.dart
@@ -50,11 +50,13 @@ class GetStartedPage extends StatelessWidget {
                     bottom: 80,
                   ),
                   child: TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/sign-up');
+                    },
                     style: TextButton.styleFrom(
                       backgroundColor: kPrimaryColor,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(17),
+                        borderRadius: BorderRadius.circular(defaultRadius),
                       ),
                     ),
                     child: Text(
