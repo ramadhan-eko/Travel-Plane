ee51b0a9f2feab98139855fca2b508ec35ce3448
ramadhan-eko <ramadhaneko027@gmail.com>
2022-04-26 08:31:28 +0700

    fitur bonus page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:airplane/ui/pages/get_started_page.dart';
 import 'package:flutter/material.dart';
+import 'ui/pages/bonus.page.dart';
+import 'ui/pages/get_started_page.dart';
 import 'ui/pages/sign_up_page.dart';
 import 'ui/pages/splash_page.dart';
 
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => SplashPage(),
         '/get-started': (context) => GetStartedPage(),
         '/sign-up': (context) => SignUpPage(),
+        '/bonus': (context) => BonusPage(),
       },
     );
   }
