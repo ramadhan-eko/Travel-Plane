97e19dc7a92c8176f334801157deb57d79a81341
ramadhan-eko <ramadhaneko027@gmail.com>
2022-04-27 12:20:42 +0700

    Fitur main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ui/pages/bonus.page.dart';
 import 'ui/pages/get_started_page.dart';
+import 'ui/pages/main_page.dart';
 import 'ui/pages/sign_up_page.dart';
 import 'ui/pages/splash_page.dart';
 
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
         '/get-started': (context) => GetStartedPage(),
         '/sign-up': (context) => SignUpPage(),
         '/bonus': (context) => BonusPage(),
+        '/main': (context) => MainPage(),
       },
     );
   }
