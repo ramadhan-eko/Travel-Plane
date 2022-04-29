dc285388214d30900d2b2091cb4aacc24cce83e0
ramadhan-eko <ramadhaneko027@gmail.com>
2022-04-30 03:23:06 +0700

    Update fitur destination card and tile

---
+++ lib/ui/widgets/destination_card.dart
@@ -0,0 +1,87 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class DestinationCard extends StatelessWidget {
+  final String name;
+  final String city;
+  final String imageUrl;
+  final double rating;
+  const DestinationCard(
+      {Key? key,
+      required this.name,
+      required this.city,
+      required this.imageUrl,
+      this.rating = 0.0})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 323,
+      margin: EdgeInsets.only(
+        left: defaultMargin,
+      ),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18),
+        color: kWhiteColor,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 180,
+            height: 220,
+            margin: EdgeInsets.only(bottom: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(18),
+              image: DecorationImage(
+                image: AssetImage(
+                  imageUrl,
+                ),
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 55,
+                height: 30,
+                decoration: BoxDecoration(
+                  color: kWhiteColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(18),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: blackTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: medium,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  city,
+                  style: greyTextStyle.copyWith(
+                    fontWeight: light,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
