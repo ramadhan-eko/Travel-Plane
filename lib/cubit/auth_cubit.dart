0be989b9c51b1046ee514f5b80ad67b75baad112
ramadhan-eko <ramadhaneko027@gmail.com>
2022-05-29 17:20:43 +0700

    Authentication firebase

---
+++ lib/cubit/auth_cubit.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+
+import 'package:airplane/models/user_model.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+}
